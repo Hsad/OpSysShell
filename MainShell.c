fe7b2352169e65586b1db7b426de024744f30132
Hsad <dashkieler@gmail.com>
2014-09-23 20:55:39 +0000

    input started

--- MainShell.c
+++ MainShell.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <limits.h>
 
-void getCurrDir(char* currentDirectory);
+
+//void getCurrDir(char* currentDirectory);
 
 int main(){
 	
@@ -18,47 +20,62 @@ int main(){
   	return EXIT_FAILURE;
   }
 
-	char *currentDirectory = NULL; 
-	getCurrDir(currentDirectory); //Need to call free() on this later
+	char *currentDirectory = "Heres ya fuckin path"; 
+	//getCurrDir(currentDirectory); //Need to call free() on this later
+	printf("hasnt errored yet");
 	char str[1000];
-	FILE *fp;	
+	char *splitStr;
+	FILE *fp = stdin;	
 
 	int InfRun = 1; // 1
-	//while (InfRun){
-		//fprintf(stdout, "\n%s",currentDirectory);
-		//if (fgets(str, 1000, fp) != NULL){
-			//if (str[0] == 'x'){
+	while (InfRun){
+		fprintf(stdout, "%s",currentDirectory);
+		if (fgets(str, 1000, fp) != NULL){
+			splitStr = strtok(str," ");
+			if (splitStr != NULL){
+				printf("%s",splitStr);
+			}
+			if (strcmp(str,"exit\0") == 1){
 				InfRun = 0;
-			//}
-			//puts(str);
-		//}
+				break;
+			}
+			printf("%s",str);
+		}
 		
-	//}
-	fclose(fp);
+	}
+	if (fp != NULL){fclose(fp); fp = NULL;}
 	
   return EXIT_SUCCESS;
 }
-
+/*
 void getCurrDir(char *currentDirectory){
 	currentDirectory = NULL;
 	free(currentDirectory); //open up the pointer, and remove chance for constant mem leak
 	int notLargeEnough = 1;
-	int currDirSize = 100;
+	int currDirSize = MAX_PATH+1;
 	char *currDir = currentDirectory;
-	char *currDirErr = NULL;
+	char *currDirErr;
+	currDirErr = NULL;
 	while (notLargeEnough){
-		//*currDirErr = getcwd(currDir, (size_t)currDirSize) ;
-		/*if (currDirErr != NULL){
+		//getcwd(currDir, (size_t)currDirSize);
+		fprintf(stderr,"about to segfault");
+		currDir = get_current_dir_name();
+		fprintf(stderr, "past segfault");
+		if (currDirr != NULL){
 			//return currDir;
 			currentDirectory = currDir;
 			notLargeEnough = 0;
 		}
-		else if (errno == ERANGE){
+		printf("%s",currDir);
+		printf("%i\n",errno);
+		printf("%i",ERANGE);
+		if (errno == ERANGE){
 			currDirSize += 100;
 		}
 		if (currDirSize > 100000){
 			fprintf(stderr, "Something is going wrong inside getCurrDir()");
+			notLargeEnough = 0;
 			//return EXIT_FAILURE;
-		}	*/
+		}	
 	}
-}
+}*/
