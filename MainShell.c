a77f9e477ef057e787e84fda67e0fb4fd96ec74d
Dash <Dashkieler@gmail.com>
2014-09-23 00:15:29 -0400

    Test2

--- MainShell.c
+++ MainShell.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+
+void getCurrDir(char);
 
 int main(){
 	
-	//int InfRun = true;
-	//while (InfRun){
-		//printf()
-	//}
 	int envSet;
 	envSet = setenv("MYHOME","/bin:.",0);
 	if (envSet != 0){ printf("MYHOME not set"); }
@@ -18,12 +17,37 @@ int main(){
   	fprintf( stderr, "$MYHOME is not set!\n" );
   	return EXIT_FAILURE;
   }
- 
-  printf( "$MYHOME is %s\n", home );
- 
-  char copy_of_home[100];  /* BIG assumption that len < 100 */
-  strcpy( copy_of_home, home );
-  printf( "$MYHOME copy is %s\n", copy_of_home );
+
+	char *currentDirectory = NULL; 
+	//getCurrDir(& currentDirectory); //Need to call free() on this later
+	
+	int InfRun = 0; // 1
+	while (InfRun){
+		fprintf(stdout, currentDirectory);
+		
+	}
 	
   return EXIT_SUCCESS;
+}
+
+void getCurrDir(char currentDirectory){
+	//free(currentDirectory); //open up the pointer, and remove chance for constant mem leak
+	//int notLargeEnough = 1;
+	//int currDirSize = 100;
+	//char *currDir = currentDirectory;
+	//char *currDirErr = NULL;
+	//while (notLargeEnough){
+		//currDirErr = getcwd(currDir, (size_t)currDirSize) ;
+		//if (currDirErr != NULL){
+			////return currDir;
+			//currentDirectory = currDir;
+			//notLargeEnough = 0;
+		//}
+		//else if (errno == ERANGE){
+			//currDirSize += 100;
+		//}
+		//if (currDirSize > 100000){
+			//fprintf(stderr, "Something is going wrong inside getCurrDir()");
+			//return EXIT_FAILURE;
+		//}
 }
